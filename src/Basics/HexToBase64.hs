552c820e3c09d9ea841984d25a321e3617d10f86
Leo Zhang <leo@leozhang.me>
2015-10-23 15:13:00 -0700

    Transition to new Util.Convert
    - Update resolver
    - More efficient padLeading
    - Intermediate representation should be ByteString, not String of binary

--- src/Basics/HexToBase64.hs
+++ src/Basics/HexToBase64.hs
@@ -1,6 +1,6 @@
 module Basics.HexToBase64 (transcode16to64) where
 
-import Util.Convert (decode16, encode64)
+import Util.Convert (decode16', encode64')
 
 transcode16to64 :: String -> String
-transcode16to64 = encode64 . decode16
+transcode16to64 = encode64' . decode16'
--- src/Util/Convert.hs
+++ src/Util/Convert.hs
@@ -2,28 +2,29 @@ module Util.Convert (
   padLeading, pad0,
   decode2, encode2,
   decode16, encode16,
+  decode16', encode16',
   decode64, encode64,
+  encode64',
   decode64Char, encode64Char
 ) where
 
 import Numeric (showIntAtBase, readInt,
                 readHex, showHex)
+
+import Data.ByteString as BS (ByteString, pack, unpack)
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Base64 as BS64
+
 import Data.Char (intToDigit, digitToInt)
 import Data.Maybe (fromMaybe)
 import Data.List (elemIndex, find)
 import Data.List.Split (chunksOf)
 
-newtype Base256 = Base256 String
-newtype Base64  = Base64 String
-newtype Base16  = Base16 String
-newtype Base2   = Base2 String
-
-type Ascii = Base256
-type Hex = Base16
-type Binary = Base2
-
 padLeading :: Char -> Int -> String -> String
-padLeading c l s = if length s < l then padLeading c l (c:s) else s
+padLeading c l s = _padLeading c (l - length s) s
+
+_padLeading :: Char -> Int -> String -> String
+_padLeading c l s = if l > 0 then c : _padLeading c (l - 1) s else s
 
 pad0 :: Int -> String -> String
 pad0 = padLeading '0'
@@ -37,8 +38,14 @@ encode2 n = showIntAtBase 2 intToDigit n ""
 decode16 :: (Integral a) => String -> a
 decode16 = fst . head . readHex
 
+decode16' :: String -> ByteString
+decode16' s = BS.pack $ map (fst . head . readHex) $ chunksOf 2 s
+
 encode16 :: (Integral a, Show a) => a -> String
 encode16 n = showHex n ""
+
+encode16' :: ByteString -> String
+encode16' b = concatMap (`showHex` "") $ BS.unpack b
 
 base64Table = ['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "+/"
 
@@ -47,6 +54,9 @@ encode64Char n = base64Table !! n
 
 encode64 :: (Integral a, Show a) => a -> String
 encode64 n = showIntAtBase 64 encode64Char n ""
+
+encode64' :: ByteString -> String
+encode64' b = BS8.unpack $ BS64.encode b
 
 decode64Char :: Char -> Int
 decode64Char c = fromMaybe (-1) $ elemIndex c base64Table
